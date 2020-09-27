aff480a19172806ecd575ca31331a39ecdddf5ea
Tim <timigntrt@icloud.com>
2020-09-27 13:55:41 +0300

    Initial Commit

---
+++ Tutorial1/Tutorial1App.swift
@@ -0,0 +1,17 @@
+//
+//  Tutorial1App.swift
+//  Tutorial1
+//
+//  Created by TIM on 9/27/20.
+//
+
+import SwiftUI
+
+@main
+struct Tutorial1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
